e23f9c032e1f45c4ab026f188507a2b618f08e85
Scott Weinstein <sweinstein27@gmail.com>
2018-05-22 18:41:45 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,7 +21,7 @@ class Transfer
       sender.balance -= amount
       receiver.balance += amount
       self.status = "complete"
-    elsif valid?
+    elsif !valid?
       self.status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
